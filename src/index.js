94dc880a74c83c78dddb1097590ea3f92bd9bf1e
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-04-02 14:40:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries'; // Іменований імпорт
+import { fetchCountries } from './fetchCountries'; 
 
 const DEBOUNCE_DELAY = 300; // затримка для дебоунс
 
